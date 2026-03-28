bf4c33e600ff6f2f453b74184019581c096f71d3
nobidigi <nobidigi@gmail.com>
2026-03-28 09:48:24 +0900

    Fix: restore search by adding output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  // ↓ この一行を追加してください
+  output: 'server', 
   adapter: vercel({
     webAnalytics: {
       enabled: true,
